9c23c65f789a78e6580657ff512574f1ce2e92c4
morrison-ben <morrisonb84@gmail.com>
2021-03-17 18:49:22 -0400

    cleanup

--- client/src/components/TopEvents.js
+++ client/src/components/TopEvents.js
@@ -3,7 +3,7 @@ import { useQuery } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { Link } from "react-router-dom";
 import Game from "../components/GameTypes/Game";
-import { Grid, Loader } from "semantic-ui-react";
+import { Grid, Loader, Header, Segment } from "semantic-ui-react";
 import { FETCH_TOP_PREGAME_EVENTS, FETCH_TOP_LIVEGAME_EVENTS } from "../util/graphql";
 
 function TopEvents() {
@@ -26,51 +26,74 @@ function TopEvents() {
   const currentDate = new Date();
 
   return (
-    <div>
+    <div style = {{
+        marginTop: '40px',
+      }}>
 
-      <h1>Top Live Games</h1>
-      <Grid stackable columns="two">
-        <Grid.Row>
-          <Fragment>
-          {
-            liveGameData.getTopLivegameEvents.filter(event => event.game).map((event) => (
-                <Grid.Column>
-                  <Link
-                    to={`/scoreboard/${event.game.league}/${event.game.gameId}`}
-                  >
-                    <span className="card" style={{ display: "block" }}>
-                      <Game key={event.game.gameId} {...event.game} />
-                    </span>
-                  </Link>
-                </Grid.Column>
-              ))
-            }
-          </Fragment>
-        </Grid.Row>
-      </Grid>
+      {liveGameData.getTopLivegameEvents.length > 0 ? (
+        <>
+        <Header as='h3'>
+          <Header.Content>Live Games</Header.Content>
+        </Header>
 
-      <h1>Top Upcoming Games</h1>
-      <Grid stackable columns="two">
-        <Grid.Row>
-          <Fragment>
-          {
-            preGameData.getTopPregameEvents.filter(event => event.game)
-            .filter(game => new Date(game.startTime) >= currentDate.setHours(currentDate.getHours()-1))
-            .map((event) => (
-                <Grid.Column>
-                  <Link
-                    to={`/scoreboard/${event.game.league}/${event.game.gameId}`}
-                  >
-                    <span className="card" style={{ display: "block" }}>
-                      <Game key={event.game.gameId} {...event.game} />
-                    </span>
-                  </Link>
-                </Grid.Column>
-              ))
-            }
-          </Fragment>
-        </Grid.Row>
-      </Grid>
+        <Segment raised>
+
+          <Grid stackable columns="two">
+            <Grid.Row>
+              <Fragment>
+              {
+                liveGameData.getTopLivegameEvents.filter(event => event.game).map((event) => (
+                    <Grid.Column>
+                      <Link
+                        to={`/scoreboard/${event.game.league}/${event.game.gameId}`}
+                      >
+                        <span className="card" style={{ display: "block" }}>
+                          <Game key={event.game.gameId} {...event.game} />
+                        </span>
+                      </Link>
+                    </Grid.Column>
+                  ))
+                }
+              </Fragment>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        </>
+      ) : (<></>)}
+
+
+      {preGameData.getTopPregameEvents.filter(event => event.game).length > 0 ? (
+        <>
+        <Header as='h3'>
+          <Header.Content>Upcoming Games</Header.Content>
+        </Header>
+
+        <Segment raised>
+
+          <Grid stackable columns="two">
+            <Grid.Row>
+              <Fragment>
+              {
+                preGameData.getTopPregameEvents.filter(event => event.game)
+                .filter(game => new Date(game.startTime) >= currentDate.setHours(currentDate.getHours()-1))
+                .map((event) => (
+                    <Grid.Column>
+                      <Link
+                        to={`/scoreboard/${event.game.league}/${event.game.gameId}`}
+                      >
+                        <span className="card" style={{ display: "block" }}>
+                          <Game key={event.game.gameId} {...event.game} />
+                        </span>
+                      </Link>
+                    </Grid.Column>
+                  ))
+                }
+              </Fragment>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+        </>
+      ) : (<></>)}
     </div>
   )
 }
