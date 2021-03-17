76d92672962f9f4898ed0da5343d64d599f0380c
morrison-ben <morrisonb84@gmail.com>
2021-03-17 14:50:11 -0400

    adding express server for heroku

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:5000'
+  uri: 'http://localhost:5000/graphql'
 });
 
 const authLink = setContext(() => {
--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const { ApolloServer, PubSub } = require("apollo-server");
+//const { ApolloServer, PubSub } = require("apollo-server");
+const { ApolloServer } = require("apollo-server-express");
 const mongoose = require("mongoose");
 const express = require('express');
 const path = require("path");
+const cors = require('cors');
 
 const { MONGODB } = require("./config.js");
 const typeDefs = require("./graphql/typedefs");
 const resolvers = require("./graphql/resolvers");
 
-const pubsub = new PubSub();
+//const pubsub = new PubSub();
 
 let updateGames = require('./services/GameService');
 let myGameService = updateGames.GameService;
 
 const PORT = process.env.PORT || 5000;
 
+// const server = new ApolloServer({
+//   playground: true,
+//   typeDefs,
+//   resolvers,
+//   context: ({ req }) => ({ req, pubsub }),
+// });
+
 const server = new ApolloServer({
-  playground: true,
   typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req, pubsub }),
+  resolvers
 });
 
 const app = express();
+app.use(cors());
+
 app.use('/',express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
@@ -33,11 +42,12 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
   .then(() => {
     console.log("MongoDB Connected");
-    return server.listen({ port: PORT });
-  })
-  .then((res) => {
-    console.log(`server running at ${res.url}`);
   });
+
+
+app.listen({ port: PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+);
 
 //start game service based on command line arg
 if (process.argv.length > 2 && process.argv[2] === "g") {
